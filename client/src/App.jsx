1da9e0d3baf8d3e23ff085c25b19c7debc36c618
Ahsan <ahsananwar14may@gmail.com>
2024-02-29 22:37:58 +0500

    creation of pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="bg-gray-200 p-4">
-      <p className="text-black-500">Hello, Tailwind CSS!</p>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/About' element={<About />}></Route>
+      <Route path='/Sign-up' element={<SignUp />}></Route>
+      <Route path='Sign-in' element={<Signin />}></Route>
+      <Route path='Dashboard' element={<Dashboard />}></Route>
+      <Route path='Projects' element={<Projects />}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
